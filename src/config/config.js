4fab4f7136d9840526afb9074eb56d265cdff509
Agung Rizka <mr.gomu@gmail.com>
2023-02-06 17:26:17 +0800

    add

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 module.exports = {
   development: {
@@ -6,20 +6,20 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
-    dialect: "mysql",
+    dialect: 'mysql',
   },
   test: {
-    username: "root",
+    username: 'root',
     password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
   production: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     host: process.env.DATABASE_HOST,
-    dialect: "mysql",
+    dialect: 'mysql',
   },
 };
